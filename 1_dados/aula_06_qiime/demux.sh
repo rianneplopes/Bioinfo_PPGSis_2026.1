8d8fac232b513bd9ba2edd6c14f773b0568bc4ef
Rianne Passos Lopes <99539363+rianneplopes@users.noreply.github.com>
2026-06-23 14:47:00 +0000

    Downloads aula 6 e pt1 Qiime

---
+++ 1_dados/aula_06_qiime/demux.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+qiime demux emp-paired \
+  --i-seqs              ./4_qza/seqs.qza \
+  --m-barcodes-file      sample-metadata.tsv \
+  --m-barcodes-column    barcode-sequence \
+  --p-rev-comp-mapping-barcodes \
+  --o-per-sample-sequences ./4_qza/demux-full.qza \
+  --o-error-correction-details ./4_qza/demux-details.qza
+# --i-seqs                      : artefato com as reads multiplexadas
+# --m-barcodes-file             : arquivo de metadados com os barcodes por amostra
+# --m-barcodes-column           : coluna do TSV que contém as sequências de barcode
+# --p-rev-comp-mapping-barcodes : os barcodes no arquivo estão no complemento reverso
+#                                 em relação ao arquivo de metadados (padrão EMP)
+# --o-per-sample-sequences      : reads separadas por amostra (artefato principal)
+# --o-error-correction-details  : estatísticas de correção de erro de barcode
