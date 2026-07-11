a04c3e859040bb36e2e1f023194dcb0ede8ffcb3
Rianne Passos Lopes <99539363+rianneplopes@users.noreply.github.com>
2026-07-11 02:11:19 +0000

    Entrega do projeto Fish eDNA metabarcoding

--- 6_projetos/rianne_silas_cecilia/3_scripts/06_demux_summarize.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export TZ="UTC"
-
-qiime demux summarize \
-    --i-data ../4_output/1_qiime_artifacts/demux_trimmed.qza \
-    --o-visualization ../4_output/1_qiime_artifacts/demux_trimmed.qzv
